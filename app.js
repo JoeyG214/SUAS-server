e45201bd797074885afdaba9eee947677939bd4c
JoeyG <erjo.guce214@gmail.com>
2023-03-01 05:30:26 -0500

    Added middleware

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express =  require('express')
+// Imports
+const express = require('express')
 const app = express()
-
-// Other Imports
+const cors = require('cors')
+const morgan = require('morgan')
 
 // Middleware
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
 
 // Routes
 
 
+// Export App
 module.exports = app
