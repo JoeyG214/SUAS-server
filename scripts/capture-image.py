91083157ebcd65da084802d4b4922e194fac3118
JoeyG214 <erjo.guce214@gmail.com>
2023-06-03 20:04:31 -0400

    remove images directory prior to camera capture

--- scripts/capture-image.py
+++ scripts/capture-image.py
@@ -1,23 +1,34 @@
 from picamera2 import Picamera2, Preview
 from datetime import datetime
+import shutil
 import time
 import os
 
+# Specifies directory where captured images are stored
 images_directory = os.path.expanduser('~/web/SUAS-server/images')
 
-if not os.path.exists('images'):
+# Removes directory if exists
+if os.path.exists(images_directory):
+    shutil.rmtree(images_directory)
+
+# Creates directory if exists
+if not os.path.exists(images_directory):
     os.makedirs(images_directory)
 
+# Camera intiialization, configuration, and preview
 picam2 = Picamera2()
 camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
 picam2.start_preview(Preview.QTGL)
+
+# Start capturing, 0.1 Sec preview before capture
 picam2.start()
-time.sleep(0.1) # 0.1 Sec preview before capture
+time.sleep(0.1)
 
 # Takes hour, minute, second time image is captured 
 timestamp = datetime.now().strftime('%H%M%S')
 
+# Stores file
 filename = f'../images/image_{timestamp}.jpg'
 picam2.capture_file(filename)
 
